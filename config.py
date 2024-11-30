03974f35c065b1e36ba68cf1c09405415d5f812e
MrKali7 <sandeepjangra1011@gmail.com>
2024-11-30 12:06:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -51,7 +51,7 @@ FORCE_SUB_CHANNEL4 = int(os.environ.get("FORCE_SUB_CHANNEL4", "0"))
 #SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "") 
 #SHORTLINK_API = os.environ.get("SHORTLINK_API", "")
 #VERIFY_EXPIRE = int(os.environ.get('VERIFY_EXPIRE', 86400)) # Add time in seconds
-#IS_VERIFY = os.environ.get("IS_VERIFY", "False")
+IS_VERIFY = os.environ.get("IS_VERIFY", "False")
 #TUT_VID = os.environ.get("TUT_VID", "https://t.me")
 
 # ignore this one
